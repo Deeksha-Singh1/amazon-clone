55a54dc429fa6c97cc2693fecb090b7faa62d1ed
Deeksha-Singh1 <deeksha.singh1_cs19@gla.ac.in>
2021-11-17 13:21:42 +0530

    ch

---
+++ amazon-clone/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import './App.css';
+import './Header';
+import Header from "./Header";
+import SearchIcon from '@mui/icons-material/Search';
+
+function App() {
+  return (
+    //BEM convention
+    <div className="App">
+     
+      {/* header */}
+
+      <Header />
+
+
+      {/*Home*/}
+    </div>
+  );
+}
+
+export default App;
+ 
